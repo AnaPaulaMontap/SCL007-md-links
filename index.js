8d5605c0da0f9aa43ea34759a12b792747093525
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-02-22 02:12:17 -0300

    revisa el archivo y extrae los link

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-module.exports = () => {
-  // ...
+/*module.exports = () => {
+  
 };
+
+*/
+
+let fs = require("fs")
+let path = require("path")
+let markdownLinkExtractor = require('markdown-link-extractor');
+let http = require('https')
+let file = process.argv[2]
+let linkArr = []
+
+
+
+const mdLinks = (file) => {
+
+fs.readFile(file, function(error , buf){ // LEE EL ARCHIVO
+    if (error){
+        console.error(error)
+    }
+    if(path.extname(file) ===  ".md") {
+      linkArr = markdownLinkExtractor(buf.toString()); // TRANSFORMA EL BUFFER EN STRING Y EXTRAE LOS LINKS
+     // links.forEach(function (link) { // RECORRE LOS LINK Y LOS EXTRAE      
+     //console.log(linkArr);
+     validation(linkArr)
+    }
+    
+  })
+}
+
+const validation = (linkArr) =>{  
+
+  let url = linkArr[0]
+  http.get(url, function(response){
+    if(response.statusCode === 200){
+      console.log(url,'status ok')
+    }
+    response.on('error', function(error){
+       console.error(error)
+    })
+}) 
+}
+
+  mdLinks(file)
+  
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,12 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
+  it('Debería ingresar un argumento', () => {
     console.log('FIX ME!');
   });
-
+  it('Debería emitir un error si la ruta no es valida', () => {
+    console.log('FIX ME!');
+  });
 });
