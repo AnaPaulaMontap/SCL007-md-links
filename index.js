bdc884f677671c0e40f12e5a4125296d5b7c062d
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-02-25 16:47:43 -0300

    Tratando de ingresar a una carpeta

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-/*module.exports = () => {
-  
-};
+#!/usr/bin/env node 
+//module.exports = mdLinks
 
-*/
 
 let fs = require("fs")
 let path = require("path")
@@ -10,17 +8,35 @@ let markdownLinkExtractor = require('markdown-link-extractor');
 let https = require('https')
 let http = require('http')
 let chalk = require('chalk')
-let file = process.argv[2]
-let linkArr = []
+let argumentsUser = process.argv[2]
 
 
+const mdLinks= {}
 
-const mdLinks = (file) => {
+mdLinks.readDirectory = (argumentsUser) => {
 
-fs.readFile(file, function(error , buf){  // LEE EL ARCHIVO
-    
+  fs.readdir( argumentsUser, function( error , list){ 
     if (error){
         console.error(error)
+    }
+    list.forEach(function(files){
+       if(fs.lstatSync(files).isDirectory()) {
+           return mdLinks.readDirectory (files)
+        }if(fs.lstatSync(files).isFile()){
+            return mdLinks.readfile(files)
+
+        }
+    })
+    })
+  }  
+  
+
+mdLinks.readfile = (file) => {
+return new Promise ((resolve, reject)=>{
+  fs.readFile(file, function(error , buf){ // LEE EL ARCHIVO
+    let linkArr = []
+    if (error){
+        return reject(error)
     }
     if(path.extname(file) ===  ".md") {
       let lineFile = buf.toString().split('\n')
@@ -38,38 +54,64 @@ fs.readFile(file, function(error , buf){  // LEE EL ARCHIVO
        }
       }   
      
-     console.log(`Existen ${linkArr.length} links en este documento`) 
-      validation(linkArr) 
+     //console.log('Existen' +' '+ chalk.magenta(linkArr.length + 'links')+ ' '+'en este documento') 
+    //validation(linkArr) 
     }
-    
+    return resolve(linkArr)
   })
+ })
 }
 
-const validation = (linkArr) => {
- 
-  linkArr.forEach((link) => {
-    if (link.url.substring(0, 5) == 'https') {
-      https.get(link.url, (response) => {
 
+mdLinks.validation = (linkArr) => {
+  return new Promise ((resolve, reject)=>{
+    let raquel = linkArr 
+
+   // raquel.forEach((link) => { 
+     for (let i = 0; i< raquel.length; i++){  
+         //console.log(raquel[i].url)
+    if (raquel[i].url.substring(0, 5) == 'https') {
+      https.get(raquel[i].url, (response) => {
+      //console.log(raquel[i].url)
         if (response.statusCode === 200) {
-          console.log(link.line +' '+ link.url +' '+ chalk.green (`OK`))
+          
+          //console.log('linea:'+raquel.line +' '+'url:'+ link.url +' '+ chalk.green (`Status: OK`))
+           raquel[i].validate = 'OK'
+           //console.log(raquel[i].validate)
         }
       }).on('error', (e) => {
-        console.error(link.line +' '+ link.url +' '+ chalk.red(`Error: ${e.message}`));
+        //console.log(link.line +' '+ link.url +' '+ chalk.red(`Error: ${e.message}`)
+        raquel[i].validate = 'ERROR'
+        
+      
       })
     } else {
-      http.get(link.url, (response) => {
+      http.get(raquel[i].url, (response) => {
         if (response.statusCode === 200) {
-          console.log(link.line +' '+ link.url +' '+ chalk.green(`OK`))
+          //console.log('linea:'+link.line +' '+'url:'+ link.url +' '+ chalk.green (`Status: OK`))
+          raquel[i].validate = 'OK'
         }
         
       }).on('error', (e) => {
-        console.error( link.line +' '+ link.url +' '+ chalk.red(`Error: ${e.message}`));
+        //console.log( link.line +' '+ link.url +' '+ chalk.red(`Error: ${e.message}`));
+        raquel[i].validate = 'ERROR'
       })
     }
-  })
-
+  }
+  return resolve(raquel)
+})
 }
 
-  mdLinks(file)
+
+
+
+  mdLinks.readfile(file)
+  .then(data =>{
+    return mdLinks.validation(data)
+  })
+  .then(lala =>{
+    console.log(lala)
+  })
+  
+  
   
